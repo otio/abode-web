197a2ddfbbf74a0b0539a5a66c9671dfeba79ce0
Jonathan A. Mitchell <ontoneio.mm@gmail.com>
2021-11-04 16:04:18 -0700

    refactor(css): update windi config file

--- windi.config.js
+++ windi.config.js
@@ -1,3 +1,30 @@
 export default {
-    
+    extend: {
+        screens: {
+          'xs': '360px',
+          'sm': '640px',
+          'md': '768px',
+          'lg': '1024px',
+          'xl': '1280px',
+          '2xl': '1536px',
+          'hd': '1920px',
+        },
+        colors: {
+        // gray: colors.coolGray,
+        // blue: colors.lightBlue,
+        // red: colors.rose,
+        // pink: colors.fuchsia,
+        },
+        fontFamily: {
+        // sans: ['Graphik', 'sans-serif'],
+        // serif: ['Merriweather', 'serif'],
+        },
+        spacing: {
+        // 128: '32rem',
+        // 144: '36rem',
+        },
+        borderRadius: {
+        // '4xl': '2rem',
+        },
+    }
 }

ef7a6d5b77983e7cb757691af824f81294a265ff
ontoneio <ontoneio.mm@gmail.com>
2021-12-08 20:57:31 -0700

    WIP

--- components/global/Footer.vue
+++ components/global/Footer.vue
@@ -1,86 +1,45 @@
 <template>
-  <div class="flex flex-col items-center">
-    <div class="row">
-      <div class="ev-logos">
-        <img
-          alt=""
-          class="logo-james-1"
-          src="https://static.overlay-tech.com/assets/09c36b6e-636b-496c-8e43-f59207a8347b.png"
-        /><img
-          alt=""
-          class="logo-james-1"
-          src="https://static.overlay-tech.com/assets/d4e2244b-624a-4908-afc0-ea1b33fb8fb6.png"
-        />
+  <div class="relative flex flex-col items-center justify-evenly px-16">
+    <div class="flex flex-row justify-around w-screen px-16">
+      <div id="ev-logos" class="flex flex-row justify-start">
+        <div class="object-cover">
+          <img alt="" src="~/static/EV-Logo-James.png" />
+        </div>
+        <div class="object-cover">
+          <img alt="" src="~/static/EV-Logo-Shivani.png" />
+        </div>
       </div>
-      <div class="cta">
-        <p class="form-cta">
-          Subscribe to receive your market report
-        </p>
-        <div class="subscription-container">
-          <input
-            class="input"
-            placeholder="Email address"
-          /><button class="button">
+      <div class="flex flex-col justify-center items-center">
+        <div><p class="">Subscribe to receive your market report</p></div>
+        <div class="flex flex-row mt-4">
+          <input class="input" placeholder="Email address" />
+          <button class="button">
             <p class="label">Subscribe</p>
           </button>
         </div>
       </div>
     </div>
-    <div class="row">
+    <div class="flex flex-row justify-around w-screen px-16 mt-16">
       <Logo />
-      <div class="contact-info">
-       
+      <div id="contact-cards" class="flex flex-row justify-evenly">
+        <SocialCard />
+        <SocialCard />
       </div>
     </div>
   </div>
 </template>
 
 <script>
-    export default {
-        
-    }
+export default {}
 </script>
 
 <style scoped>
 .row {
-    @apply flex flex-row content-evenly;
-}
-.ev-logos {
-  @apply flex items-start; 
-}
-.logo-james-1 {
-  flex: 1;
-  align-self: stretch;
-  object-fit: cover;
-}
-.logo-james-1:not(:last-of-type) {
-  margin-right: 204px;
-}
-.cta {
-  display: flex;
-  flex-direction: column;
-  align-items: flex-end;
-  width: 467px;
-}
-.form-cta {
-  color: var(--black-primary);
-  align-self: stretch;
-  text-align: right;
-  margin-bottom: 24px;
-  font-family: var(--inter-24-bold-family);
-  font-size: var(--inter-24-bold-size);
-  font-weight: var(--inter-24-bold-weight);
-  line-height: var(--inter-24-bold-line-height);
-}
-.bottom-row {
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  align-self: stretch;
+  @apply flex flex-row justify-evenly w-screen p-7;
 }
 .input {
-  width: 268px;
-  height: 19px;
+  width: 25rem;
+  height: 3rem;
   background-color: var(--white);
   margin-right: 8px;
   border-radius: 3px;
@@ -95,11 +54,11 @@
 .button {
   background-color: var(--primary-default);
   border-radius: 3px;
-  padding: 16px 34px 16px 33px;
+  padding: 1rem 2rem;
   display: flex;
   align-items: center;
   flex: 1;
-  height: 19px;
+  height: 3rem;
   cursor: pointer;
 }
 .label {
@@ -108,37 +67,5 @@
   font-size: var(--inter-16-semibold-size);
   font-weight: var(--inter-16-semibold-weight);
   line-height: var(--inter-16-semibold-line-height);
-}
-.abode-logo {
-  padding: 0 0 0.01px;
-  display: flex;
-  flex-direction: column;
-  align-items: flex-start;
-  justify-content: center;
-  flex: 1;
-}
-.abode-letters {
-  margin-bottom: 7px;
-  padding: 0 133.18px 0 0;
-  position: relative;
-}
-.vector {
-  width: 100%;
-  height: 100%;
-}
-.bode-small {
-  width: 126.69px;
-  height: 50.42px;
-  position: absolute;
-  right: 0.01px;
-  bottom: -0.01px;
-}
-.real-estate {
-  width: 140.27px;
-  height: 18.89px;
-}
-.contact-info {
-  flex: 1;
-  position: relative;
 }
 </style>
--- components/global/SocialCard.vue
+++ components/global/SocialCard.vue
@@ -1,111 +1,90 @@
 <template>
-    <div>
-         <div class="jim-social">
-          <p class="menu-item">
-            James Caldwell<br />
-            &#43;1 415 407 2525<br />
-            james.caldwell&#64;evrealestate.com
-          </p>
-          <div class="social-icons">
-            <div class="logo-linkedin">
-              <img
-                alt=""
-                class="vector-two"
-                src="https://static.overlay-tech.com/assets/a299b0ae-30a7-45c9-8804-5a5adadd92e2.svg"
-              />
-            </div>
-            <div class="logo-facebook">
-              <img
-                alt=""
-                class="vector-two"
-                src="https://static.overlay-tech.com/assets/c5dcaae9-476e-4ef7-bc71-c0f51358efba.svg"
-              />
-            </div>
-            <div class="logo-twitter">
-              <img
-                alt=""
-                class="vector-two"
-                src="https://static.overlay-tech.com/assets/0ad2cfff-b4df-4ecc-846b-2f9c6eeff05f.svg"
-              />
-            </div>
-            <img
-              alt=""
-              class="logo-instagram"
-              src="https://static.overlay-tech.com/assets/84d735fd-03e0-47d6-a4b4-69b4ad1338d7.svg"
-            />
-          </div>
-        </div>
-        <div class="shivani-social">
-          <p class="menu-item-two">
-            Shivani Desai <br />
-            &#43;1 415 632 7599<br />
-            shivani.desai&#64;evrealestate.com
-          </p>
-          <div class="social-icons">
-            <div class="logo-linkedin">
-              <img
-                alt=""
-                class="vector-two"
-                src="https://static.overlay-tech.com/assets/6ef978c2-dc37-4433-914c-76dcd37a66b0.svg"
-              />
-            </div>
-            <div class="logo-facebook">
-              <img
-                alt=""
-                class="vector-two"
-                src="https://static.overlay-tech.com/assets/7de84ccd-4cc9-4eeb-8efb-677280d2e145.svg"
-              />
-            </div>
-            <div class="logo-twitter">
-              <img
-                alt=""
-                class="vector-two"
-                src="https://static.overlay-tech.com/assets/f02df7f4-a8f6-4de8-8d61-00cdac20a18b.svg"
-              />
-            </div>
-            <img
-              alt=""
-              class="logo-instagram"
-              src="https://static.overlay-tech.com/assets/faac359c-82d9-4ae9-a813-c97dc86e4b66.svg"
-            />
-          </div>
-        </div>
-
+  <div class="social">
+    <p class="menu-item">
+      {{ socialCard.name }}<br />
+      {{ socialCard.phone }}<br />
+      {{ socialCard.email }}
+    </p>
+    <div class="social-icons">
+      <a :href="socialCard.profiles.linkedIn" class="logo-linkedin">
+        <img
+          alt=""
+          class="vector-two"
+          src="https://static.overlay-tech.com/assets/a299b0ae-30a7-45c9-8804-5a5adadd92e2.svg"
+        />
+      </a>
+      <a :href="socialCard.profiles.fb" class="logo-facebook">
+        <img
+          alt=""
+          class="vector-two"
+          src="https://static.overlay-tech.com/assets/c5dcaae9-476e-4ef7-bc71-c0f51358efba.svg"
+        />
+      </a>
+      <a :href="socialCard.profiles.twitter" class="logo-twitter">
+        <img
+          alt=""
+          class="vector-two"
+          src="https://static.overlay-tech.com/assets/0ad2cfff-b4df-4ecc-846b-2f9c6eeff05f.svg"
+        />
+      </a>
+      <a :href="socialCard.profiles.instagram" class="logo-instagram">
+        <img
+          alt=""
+          class="vector-2"
+          src="https://static.overlay-tech.com/assets/84d735fd-03e0-47d6-a4b4-69b4ad1338d7.svg"
+        />
+      </a>
     </div>
+  </div>
 </template>
 
 <script>
-    export default {
-        name: 'SocialCard',
-        data() {
-            return {
-                socialCards: [
-                    {
-                        name: '',
-                        phone: '',
-                        email: '',
-                        profiles: [
-                            { linkedIn: '', icon: '' },
-                            { fb: '', icon: '' },
-                            { twitter: '', icon: '' },
-                            { instagram: '', icon: '' }
-                        ]
-                    }
-                ]
-            }
-        },
+export default {
+  name: 'SocialCard',
+  props: {
+    cardInfo: {
+      type: Object,
+      default: () => ({
+        name: 'Agent Name',
+        phone: '(555) 555-5555',
+        email: 'agent@agency.email',
+        profiles: [
+          { linkedIn: 'https://www.linkedin.com/' },
+          { fb: 'https://www.facebook.com/' },
+          { twitter: 'https://twitter.com/' },
+          { instagram: 'https://www.instagram.com/' },
+        ],
+      }),
+    },
+  },
+  data() {
+    return {
+      socialCard: {
+        name: this.cardInfo.name,
+        phone: this.cardInfo.phone,
+        email: this.cardInfo.email,
+        profiles: [
+          { linkedIn: this.cardInfo.profiles.linkedIn },
+          { fb: this.cardInfo.profiles.fb },
+          { twitter: this.cardInfo.profiles.twitter },
+          { instagram: this.cardInfo.profiles.instagram },
+        ],
+      },
     }
+  },
+}
 </script>
 
 <style scoped>
-.jim-social {
+.social {
   display: flex;
   flex-direction: column;
   align-items: flex-start;
   justify-content: center;
-  position: absolute;
+  margin: 0rem 4rem;
+  /* position: absolute;
   left: -88.5px;
-  top: 0;
+  top: 0; */
 }
 .menu-item {
   max-width: 383px;
@@ -154,20 +133,4 @@
   width: 40px;
   height: 40px;
 }
-.shivani-social {
-  display: flex;
-  flex-direction: column;
-  align-items: flex-start;
-  justify-content: center;
-}
-.menu-item-two {
-  max-width: 364px;
-  color: var(--black-secondary);
-  margin-bottom: 25px;
-  font-family: var(--inter-24-regular-family);
-  font-size: var(--inter-24-regular-size);
-  font-weight: var(--inter-24-regular-weight);
-  line-height: var(--inter-24-regular-line-height);
-}
-
 </style>

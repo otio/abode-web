c787223785e772b3b2981556c9c4f8ef6c7973eb
ontoneio <ontoneio.mm@gmail.com>
2021-12-21 17:36:13 -0800

    refactor(yarn/ui): WIP

--- components/home/FeaturedListing.vue
+++ components/home/FeaturedListing.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="featured-listing" class="px-48">
-    <div id="listing-content" class="flex flex-row">
-      <div id="listing-details" class="flex flex-col items-start">
+    <div id="listing-content" class="flex flex-row justify-around">
+      <div id="listing-details" class="w-sm inline-flex flex-col items-start">
         <div id="listing-text" class="flex flex-col">
           <p class="font-secondary text-5xl pb-12">Featured Listings</p>
           <p class="font-body text-3xl leading-loose">
@@ -18,16 +18,17 @@
               <p class="feature-label">Beds</p>
             </div>
           </div>
+          <!--  FOR VISUAL -->
           <div class="list-feature">
             <div class="feature-content">
               <p class="feature-value">1</p>
-              <p class="feature-label">Beds</p>
+              <p class="feature-label">Baths</p>
             </div>
           </div>
           <div class="list-feature">
             <div class="feature-content">
               <p class="feature-value">1</p>
-              <p class="feature-label">Beds</p>
+              <p class="feature-label">Sq. Ft</p>
             </div>
           </div>
           <div class="list-feature">
@@ -38,61 +39,16 @@
           </div>
         </div>
       </div>
-      <div id="featured-listing-image">
+      <div id="featured-listing-image" class="">
         <img
           alt=""
-          class="listing-image"
+          id="listing-image"
+          class=""
           src="https://static.overlay-tech.com/assets/88d96045-a452-4f0f-87f1-f7bbe1b8486b.png"
         />
       </div>
     </div>
   </div>
-  <!-- <div class="featured-listing">
-    <div class="listing-content">
-      <div class="listing-details">
-        <div class="listing-text">
-          <p class="heading">Featured Listings</p>
-          <p class="body">
-            Dare to be inspired by exploring this curated portfolio of some of
-            the finest real estate listings currently represented by Engel &amp;
-            Völkers.
-          </p>
-        </div>
-        <a class="listing-address">60 Ora Way #H303, San Francisco, CA 94131</a>
-        <div class="listing-features">
-          <div class="list-feature">
-            <div class="feature-content">
-              <p class="feature-value">1</p>
-              <p class="feature-label">Beds</p>
-            </div>
-          </div>
-          <div class="list-feature">
-            <div class="feature-content">
-              <p class="feature-value">1</p>
-              <p class="feature-label">Beds</p>
-            </div>
-          </div>
-          <div class="list-feature">
-            <div class="feature-content">
-              <p class="feature-value">1</p>
-              <p class="feature-label">Beds</p>
-            </div>
-          </div>
-          <div class="list-feature">
-            <div class="feature-content">
-              <p class="feature-value">1</p>
-              <p class="feature-label">Beds</p>
-            </div>
-          </div>
-        </div>
-      </div>
-      <img
-        alt=""
-        class="listing-image"
-        src="https://static.overlay-tech.com/assets/88d96045-a452-4f0f-87f1-f7bbe1b8486b.png"
-      />
-    </div>
-  </div> -->
 </template>
 
 <script>
@@ -196,8 +152,9 @@ export default {}
   font-weight: var(--hanuman-24-bold-weight);
   line-height: var(--hanuman-24-bold-line-height);
 }
-.listing-image {
-  width: calc(100% - 780px);
-  height: 580px;
+  */
+/* .listing-image {
+  width: 100%;
+  height: 100%;
 } */
 </style>

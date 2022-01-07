acf5ad094ed313d9b9198170b4641b5c0bf97c8a
ontoneio <ontoneio.mm@gmail.com>
2022-01-06 16:51:41 -0800

    refactor(nuxt): adds debugging config, adds source-maps

--- nuxt.config.js
+++ nuxt.config.js
@@ -86,5 +86,11 @@ export default {
   content: {},
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
-  build: {},
+  build: {
+    extend(config, ctx) {
+      if (ctx.isDev) {
+        config.devtool = ctx.isClient ? 'source-map' : 'inline-source-map'
+      }
+    },
+  },
 }

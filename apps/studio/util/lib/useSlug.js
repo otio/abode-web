d0fe4dc1d5ea7d5ffc7b10890fb34a15fd808528
ontoneio <ontoneio.mm@gmail.com>
2022-01-28 07:57:10 -0800

    refactor(studio): cha975642nge useSlug Hook

--- apps/studio/util/lib/useSlug.js
+++ apps/studio/util/lib/useSlug.js
@@ -24,51 +24,50 @@ export default function (document) {
   const documentType = documentState._type;
 
   console.log("1. GET DOCUMENT TYPE PREVIEW URL", documentState);
+  console.log("1.5 - Drafts", draft);
+  console.log("1.5 - Displayed", displayed);
 
   console.log(` Base Preview URL`, BasePreviewUrl);
 
   console.log("2. DOCUMENT STATE:\n", documentState);
 
+  async function fetchRoute(documentState) {
+    console.log("PAGE-ROUTE ==> :", documentState);
+    /**
+     * Since draft docs don't have access to parent references
+     * this checks for drafts and returns parent reference with a strippedId
+     */
+    // if document Id begins with "drafts" return strippedId
+    // else return id
+    const { slug, _id } = documentState;
+
+    // const isDraft = documentState["_id"].startsWith("drafts.");
+    // const trimmedPageId = isDraft
+    //   ? documentState._id.replace("drafts.", "")
+    //   : documentState._id;
+
+    // // debugger
+
+    const query = `*[ _id == $pageId]{"slug": slug.current}[0]`;
+    const params = { pageId: _id };
+
+    try {
+      const pageSlugResult = await previewClient.fetch(query, params);
+
+      console.log("ROUTE SLUG FOR PAGE =>", pageSlugResult);
+      if (slug.current === "index" || slug.current === "/") {
+        setPreviewUrl(`${BasePreviewUrl}${previewQuery}`);
+      } else {
+        setPreviewUrl(`${BasePreviewUrl}/${slug.current}${previewQuery}`);
+      }
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
   useEffect(() => {
     switch (documentType) {
       case "page":
-        async function fetchRoute(documentState) {
-          console.log("PAGE-ROUTE ==> :", documentState);
-          /**
-           * Since draft docs don't have access to parent references
-           * this checks for drafts and returns parent reference with a strippedId
-           */
-          // if document Id begins with "drafts" return strippedId
-          // else return id
-
-          const isDraft = documentState["_id"].startsWith("drafts.");
-          const trimmedPageId = isDraft
-            ? documentState._id.replace("drafts.", "")
-            : documentState._id;
-
-          // debugger
-
-          const query = `*[ _id == $pageId]{"slug": slug.current}`;
-          const params = { pageId: documentState._id };
-
-          try {
-            const pageSlugResult = await previewClient.fetch(query, params);
-
-            console.log("ROUTE SLUG FOR PAGE =>", pageSlugResult);
-            if (
-              pageSlugResult.slug === "index" ||
-              pageSlugResult.slug === "/"
-            ) {
-              setPreviewUrl(`${BasePreviewUrl}${previewQuery}`);
-            } else {
-              setPreviewUrl(
-                `${BasePreviewUrl}/${pageSlugResult.slug}${previewQuery}`
-              );
-            }
-          } catch (error) {
-            console.error(error);
-          }
-        }
         const pageSlug = fetchRoute(documentState);
         setPreviewUrl(pageSlug);
         break;

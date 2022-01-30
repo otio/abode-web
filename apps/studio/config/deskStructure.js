31d41e01a09f532555ef0efa088f24877bfb8caa
ontoneio <ontoneio.mm@gmail.com>
2022-01-29 17:49:29 -0800

    WIP

--- apps/studio/config/deskStructure.js
+++ apps/studio/config/deskStructure.js
@@ -3,37 +3,37 @@ import React from "react";
 import S from "@sanity/desk-tool/structure-builder";
 import useSlug from "../util/lib/useSlug";
 
-export const getDefaultDocumentNode = ({ schemaType, documentId }) => {
-  // Conditionally return a different configuration based on the schema type
-  switch (schemaType) {
-    case "page":
-      return S.document().views([
-        S.view.form(),
-        // S.view.editor(),
-        S.view.component(WebPreview).title("Web Preview"),
-      ]);
-      break;
+// export const getDefaultDocumentNode = ({ schemaType, documentId }) => {
+//   // Conditionally return a different configuration based on the schema type
+//   switch (schemaType) {
+//     case "page":
+//       return S.document().views([
+//         S.view.form(),
+//         // S.view.editor(),
+//         S.view.component(WebPreview).title("Web Preview"),
+//       ]);
+//       break;
 
-    default:
-      break;
-  }
-};
+//     default:
+//       break;
+//   }
+// };
 
-const WebPreview = ({ document }) => {
-  const targetURL = useSlug(document);
-  console.log("PREVIEW URL: =>", targetURL);
+// const WebPreview = ({ document }) => {
+//   const targetURL = useSlug(document);
+//   console.log("PREVIEW URL: =>", targetURL);
 
-  if (targetURL !== null || targetURL !== undefined) {
-    return (
-      <iframe src={targetURL} frameBorder={0} width="100%" height="100%" />
-    );
-  }
-  return (
-    <section width="100%" height="100%">
-      <h1>NO SLUG SET FOR CONTENT PREVIEW</h1>
-    </section>
-  );
-};
+//   if (targetURL !== null || targetURL !== undefined) {
+//     return (
+//       <iframe src={targetURL} frameBorder={0} width="100%" height="100%" />
+//     );
+//   }
+//   return (
+//     <section width="100%" height="100%">
+//       <h1>NO SLUG SET FOR CONTENT PREVIEW</h1>
+//     </section>
+//   );
+// };
 
 export default () =>
   S.list()
@@ -59,7 +59,7 @@ export default () =>
                 .documentId(docId)
                 .views([
                   S.view.form(),
-                  S.view.component(WebPreview).title("Web Preview"),
+                  // S.view.component(WebPreview).title("Web Preview"),
                 ])
             )
         ),

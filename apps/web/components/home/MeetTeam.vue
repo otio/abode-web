546f846871fb961a46701496e626b7cf0d34f234
ontoneio <ontoneio.mm@gmail.com>
2022-02-11 14:58:51 -0800

    style(apps/web/components): updates mobile responsiveness
    WIP

--- apps/web/components/home/MeetTeam.vue
+++ apps/web/components/home/MeetTeam.vue
@@ -1,38 +1,43 @@
 <template>
   <div class="flex flex-col items-center justify-center my-24">
     <!-- Custom Header -->
-    <div class="flex flex-row items-center justify-center self-stretch mb-16">
+    <div
+      class="flex flex-row items-center justify-center self-stretch mb-16 sm:(flex-col) xs:(flex-col)"
+    >
       <h2 class="hidden">Meet the Abode Team</h2>
       <p class="flex items-center mr-8 font-display text-6xl">Meet the</p>
-      <div class="flex flex-row items-center">
-        <img alt="" class="mr-1" src="~/static/abode-minimal-logo.svg" />
-        <p class="font-display text-6xl">bode Team</p>
+      <div class="flex flex-row items-center font-display text-6xl mr-4">
+        <div class="flex flex-row items-center">
+          <img alt="" class="mr-1" src="~/static/abode-minimal-logo.svg" />
+          <p>bode</p>
+        </div>
       </div>
+      <p class="font-display text-6xl">Team</p>
     </div>
     <!-- Custom Header END -->
     <div
-      class="flex flex-row justify-center items-start self-stretch mx-36 <xl:(flex-col-reverse items-center)"
+      class="flex flex-row justify-center items-center mx-36 xl:(flex-col items-center) lg:(flex-col items-center) md:(flex-col items-center) sm:(flex-col items-center) xs:(flex-col items-center)"
     >
-      <picture id="team-image" class="mr-16">
-        <source
-          media="(min-width:1920px)"
-          :srcset="`${imgSize(1000)} 1000w`"
-          sizes="100vw"
-        />
-        <img
-          src="https://via.placeholder.com/1000x1300.webp?text=Abode+Team+Image"
-          alt=""
-          class="mr-16"
-        />
-      </picture>
-      <!-- <img alt="" class="mr-16" src="~/static/team-intro-photo.png" /> -->
+      <img
+        alt=""
+        class="hd:(mr-16) 2xl:(mr-16) xl:(mt-0) lg:(mt-0) md:(mt-0) sm:(mt-0) xs:(mt-0)"
+        :srcset="`
+        ${imgSize(577)} 360w, 
+        ${imgSize(577, 700)} 1024w, 
+        ${imgSize(577, 700)} 1920w`"
+        sizes="(min-width: 1920px) 100vw, (min-width: 1536px) 150vw, (min-width: 1280px) 200vw, (min-width: 1024px) 200vw, (min-width: 768px) 300vw, (min-width: 640px) 200vw, (min-width: 360px) 200vw"
+      />
 
       <div class="flex flex-col items-center">
-        <div class="flex flex-col items-start pb-16">
-          <h2 class="font-secondary text-5xl 2xl:text-4xl pb-16">
+        <div class="flex flex-col items-start pb-16 xl:(items-center)">
+          <h2
+            class="font-secondary pb-16 text-5xl 2xl:(text-4xl) sm:(text-2xl)"
+          >
             {{ $attrs.introText.headline }}
           </h2>
-          <p class="font-body text-3xl leading-loose">
+          <p
+            class="font-body text-3xl leading-loose xs:(text-base, leading-normal)"
+          >
             {{ $attrs.introText.paragraphText }}
           </p>
         </div>
@@ -68,8 +73,4 @@ export default {
 }
 </script>
 
-<style scoped>
-#team-image > img {
-  width: 100%;
-}
-</style>
+<style scoped></style>

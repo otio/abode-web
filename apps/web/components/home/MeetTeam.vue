d23c9b4b55fdeee7de594bf07ae328c8f6748753
ontoneio <ontoneio.mm@gmail.com>
2022-02-11 16:15:52 -0800

    refactor(apps/web/components/home): styles to meet responsive goals

--- apps/web/components/home/MeetTeam.vue
+++ apps/web/components/home/MeetTeam.vue
@@ -16,7 +16,7 @@
     </div>
     <!-- Custom Header END -->
     <div
-      class="flex flex-row justify-center items-center mx-36 xl:(flex-col items-center) lg:(flex-col items-center) md:(flex-col items-center) sm:(flex-col items-center) xs:(flex-col items-center)"
+      class="flex flex-row justify-center items-center mx-36 xl:(flex-col items-center) lg:(flex-col items-center) md:(flex-col items-center) sm:(flex-col items-center) xs:(flex-col items-center mx-0)"
     >
       <img
         alt=""
@@ -25,18 +25,18 @@
         ${imgSize(577)} 360w, 
         ${imgSize(577, 700)} 1024w, 
         ${imgSize(577, 700)} 1920w`"
-        sizes="(min-width: 1920px) 100vw, (min-width: 1536px) 150vw, (min-width: 1280px) 200vw, (min-width: 1024px) 200vw, (min-width: 768px) 300vw, (min-width: 640px) 200vw, (min-width: 360px) 200vw"
+        sizes="(min-width: 1920px) 100vw, (min-width: 1536px) 150vw, (min-width: 1280px) 200vw, (min-width: 1024px) 200vw, (min-width: 768px) 300vw, (min-width: 640px) 200vw, (min-width: 360px) 300vw"
       />
 
       <div class="flex flex-col items-center">
-        <div class="flex flex-col items-start pb-16 xl:(items-center)">
-          <h2
-            class="font-secondary pb-16 text-5xl 2xl:(text-4xl) sm:(text-2xl)"
-          >
-            {{ $attrs.introText.headline }}
+        <div
+          class="flex flex-col items-start pb-16 xl:(items-center) sm:(mx-0) xs:(mx-8)"
+        >
+          <h2 class="font-secondary pb-16 text-5xl sm:(text-4xl) xs:(text-4xl)">
+            {{ headline }}
           </h2>
           <p
-            class="font-body text-3xl leading-loose xs:(text-base, leading-normal)"
+            class="font-body text-3xl leading-loose sm:(text-2xl leading-normal) xs:(text-2xl leading-normal)"
           >
             {{ $attrs.introText.paragraphText }}
           </p>
@@ -56,6 +56,11 @@ export default {
     return {
       teamImageId: this.$attrs?.teamImage?.public_id,
     }
+  },
+  computed: {
+    headline() {
+      return this.$attrs?.introText?.headline
+    },
   },
   methods: {
     imgSize(imageWidth, imageHeight = 'auto') {

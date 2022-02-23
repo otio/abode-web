07d79b09d7956f2f3b741d6e0948b96e7ad3b991
ontoneio <ontoneio.mm@gmail.com>
2022-02-23 10:48:27 -0800

    refactor(apps/studio): adds config for netlify build widget

--- apps/studio/config/dashboardConfig.js
+++ apps/studio/config/dashboardConfig.js
@@ -9,5 +9,20 @@ export default {
     {
       name: "project-users",
     },
+    {
+      name: "netlify",
+      options: {
+        title: "Abode Sites",
+        sites: [
+          {
+            title: "aboderealty.group",
+            apiId: process.env.SANITY_STUDIO_NETLIFY_API_ID_ABODE_MAIN,
+            buildHookId: process.env.SANITY_STUDIO_NETLIFY_ABODE_MAIN_HOOK_ID,
+            name: "abode-sf",
+            url: "https://aboderealty.group/",
+          },
+        ],
+      },
+    },
   ],
 };

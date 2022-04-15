5b2c2cebcbfae5088999b2b114f70b6d8e988f76
Jonathan A. Mitchell <ontoneio.mm@gmail.com>
2022-04-14 17:00:42 -0700

    refactor(studio): adds development site to Netlify deploy

--- apps/studio/config/dashboardConfig.js
+++ apps/studio/config/dashboardConfig.js
@@ -15,10 +15,17 @@ export default {
         title: "Abode Sites",
         sites: [
           {
-            title: "aboderealty.group",
+            title: "Development - Non-Public - develop.aboderealty.group",
+            apiId: process.env.SANITY_STUDIO_NETLIFY_API_ID_ABODE_MAIN,
+            buildHookId: process.env.SANITY_STUDIO_NETLIFY_ABODE_DEV_HOOK_ID,
+            name: "abode-sf Development",
+            url: "https://develop.aboderealty.group/",
+          },
+          {
+            title: "Production - Public Site - www.aboderealty.group",
             apiId: process.env.SANITY_STUDIO_NETLIFY_API_ID_ABODE_MAIN,
             buildHookId: process.env.SANITY_STUDIO_NETLIFY_ABODE_MAIN_HOOK_ID,
-            name: "abode-sf",
+            name: "abode-sf Production",
             url: "https://aboderealty.group/",
           },
         ],
